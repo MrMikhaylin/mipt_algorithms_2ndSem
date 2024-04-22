e30ff1ededc9596bb7623c0a741a39a3a781e897
MrMikhaylin <mikhailin.ka@phystech.edu>
2024-04-23 01:37:41 +0300

    Изменения 23.04.2024

--- Contests/Contest_1/A.c
+++ Contests/Contest_1/A.c
@@ -1,88 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include <assert.h>
+
+typedef int elem_t; 
 
 struct stack
 {
-    int data[10000];
-    int size;
-    int capacity;
+    elem_t* data;
+    size_t size;
+    size_t capacity;
 };
 
-struct stack* st_ctor ()
+struct stack* st_ctor (size_t elem_size)
 {
     struct stack* stk = (struct stack*) calloc (1, sizeof (struct stack));
     assert (stk != NULL);
 
     stk->capacity = 10000;
     stk->size = 0;
+    
+    stk->data = (elem_t*) calloc (stk->capacity, elem_size);
+    assert (stk->data != NULL);
 
     return stk;
 }
 
-void st_push (struct stack* stk, int number, FILE* dst)
+void st_push (struct stack* stk, elem_t number)
 {
-    assert ((stk != NULL) && (dst != NULL));
+    assert (stk != NULL);
+
+    if (stk->size == stk->capacity)
+    {
+        stk->data = (elem_t*) realloc ((elem_t *)stk->data, 2 * stk->capacity * sizeof (elem_t));
+        stk->capacity *= 2;
+    }
 
     stk->data[stk->size] = number;
     stk->size++;
-
-    fprintf (dst, "ok\n"); 
 }
 
-void st_pop (struct stack* stk, FILE* dst)
+void st_pop (struct stack* stk, elem_t* number)
 {
-    assert ((stk != NULL) && (dst != NULL));
+    assert (stk != NULL);
 
-    if ((stk->size) == 0)
-        fprintf (dst, "error\n");
-            
-    else
+    if (stk->size <= stk->capacity/4)
     {
-        int number = stk->data[stk->size - 1];
-        stk->data[stk->size - 1] = 0;
-        stk->size--;
-                
-        fprintf (dst, "%d\n", number);
+        
+        stk->data = (elem_t*) realloc ((elem_t*) stk->data, stk->capacity * sizeof (elem_t)/2);
+        stk->capacity /= 2;
     }
+
+    if (stk->size >= 0)
+    {
+        *number = stk->data[stk->size - 1];
+        stk->size--;
+    }
+
+    else
+        assert (false);
 }
 
-void st_clear (struct stack* stk, FILE* dst)
+void st_clear (struct stack* stk)
 {
-    assert ((stk != NULL) && (dst != NULL));
+    assert (stk != NULL);
 
     for (int i = stk->size - 1; i >= 0; i--)
         stk->data[i] = 0;
             
     stk->size = 0;
-
-    fprintf (dst, "ok\n");
 }
 
-void st_back (struct stack* stk, FILE* dst)
+void st_back (struct stack* stk, elem_t* number)
 {
-    assert ((stk != NULL) && (dst != NULL));
+    assert (stk != NULL);
 
-    if ((stk->size) == 0)
-        fprintf (dst, "error\n");
-            
-    else
-    {
-        int number = stk->data[stk->size - 1];
-                
-        fprintf (dst, "%d\n", number);
-    }
+    *number = stk->data[stk->size - 1];
 }
 
 int main()
 {
-    struct stack* stk = st_ctor ();
+    struct stack* stk = st_ctor (sizeof (elem_t));
 
     char* command = (char*) calloc (10, sizeof (char));
     assert (command != NULL);
 
-    int number = 0;
+    elem_t number = 0;
 
     FILE* dst = fopen ("output.txt", "w");
     assert (dst != NULL);
@@ -95,24 +99,42 @@ int main()
         {
             scanf (" %d\n", &number);
 
-            st_push (stk, number, dst);
+            st_push (stk, number);
+            fprintf (dst, "ok\n"); 
         }
 
         else
             if (strcmp (command, "pop") == 0)
-                st_pop (stk, dst);
+                if (stk->size == 0)
+                    fprintf (dst, "error\n");
+                
+                else
+                {
+                    st_pop (stk, &number);
+                    fprintf (dst, "%d\n", number);
+                }
+                    
 
             else
                 if (strcmp (command, "back") == 0)
-                    st_back (stk, dst);
-
+                    if ((stk->size) == 0)
+                        fprintf (dst, "error\n");
+                    
+                    else
+                    {
+                        st_back (stk, &number);
+                        fprintf (dst, "%d\n", number);
+                    }
                 else
                     if (strcmp (command, "size") == 0)
                         fprintf (dst, "%d\n", stk->size);
         
                     else
                         if (strcmp (command, "clear") == 0)
-                            st_clear (stk, dst);
+                        {
+                            st_clear (stk);
+                            fprintf (dst, "ok\n");
+                        }
 
                         else
                             if (strcmp (command, "exit") == 0)
