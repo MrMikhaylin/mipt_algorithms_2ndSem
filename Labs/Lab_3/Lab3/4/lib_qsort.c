771e6e0f1e9a18d24c30ca2e4b0008cf92c27d4d
MrMikhaylin <mikhailin.ka@phystech.edu>
2024-04-30 14:23:02 +0300

    Исправил int_comparator в lib_qsort

--- Labs/Lab_3/Lab3/4/lib_qsort.c
+++ Labs/Lab_3/Lab3/4/lib_qsort.c
@@ -8,14 +8,8 @@ int int_comparator (const void* first, const void* second)
 {
     const int* a = (const int*) first;
     const int* b = (const int*) second;
-    
-    if (*a == *b)
-        return 0;
 
-    if (*a < *b)
-        return -1;
-    
-    return 1;
+    return (*a == *b) ? 0 : (*a < *b) ? -1 : 1;
 }
 
 void lib_qsort (int* array, size_t arr_length)
